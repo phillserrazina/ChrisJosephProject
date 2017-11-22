f4a69a1eb0f345bd108ce2f1ba7a8ef87b276cba
phillserrazina <fserrazina99@gmail.com>
2017-11-22 22:30:31 +0000

    22/11/2017
    - Spawn up to 5 players
    - Kill Player
    - Spectate other players
    - Esc to reset

---
+++ ChrisJoseph Game Demo/Assets/Scripts/PlayerSpawn.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour {
+
+	// VARIABLES
+
+	public bool twoPlayers = false;
+	public bool threePlayer = false;
+	public bool fourPlayer = false;
+	public bool fivePlayer = false;
+
+	public GameObject[] playerPrefab;
+	public GameObject enemyPrefab;
+
+	// FUNCTIONS
+
+	public void SpawnTwoPlayers ()
+	{
+		Instantiate(playerPrefab[0], playerPrefab[0].transform.position, playerPrefab[0].transform.rotation);
+
+		Instantiate(enemyPrefab, enemyPrefab.transform.position, enemyPrefab.transform.rotation);
+
+		twoPlayers = true;
+	}
+
+	public void SpawnThreePlayers ()
+	{
+		Instantiate(playerPrefab[0], playerPrefab[0].transform.position, playerPrefab[0].transform.rotation);
+		Instantiate(playerPrefab[1], playerPrefab[1].transform.position, playerPrefab[1].transform.rotation);
+
+		Instantiate(enemyPrefab, enemyPrefab.transform.position, enemyPrefab.transform.rotation);
+
+		threePlayer = true;
+	}
+
+	public void SpawnFourPlayers ()
+	{
+		Instantiate(playerPrefab[0], playerPrefab[0].transform.position, playerPrefab[0].transform.rotation);
+		Instantiate(playerPrefab[1], playerPrefab[1].transform.position, playerPrefab[1].transform.rotation);
+		Instantiate(playerPrefab[2], playerPrefab[2].transform.position, playerPrefab[2].transform.rotation);
+
+		Instantiate(enemyPrefab, enemyPrefab.transform.position, enemyPrefab.transform.rotation);
+
+		fourPlayer = true;
+	}
+
+	public void SpawnFivePlayers ()
+	{
+		Instantiate(playerPrefab[0], playerPrefab[0].transform.position, playerPrefab[0].transform.rotation);
+		Instantiate(playerPrefab[1], playerPrefab[1].transform.position, playerPrefab[1].transform.rotation);
+		Instantiate(playerPrefab[2], playerPrefab[2].transform.position, playerPrefab[2].transform.rotation);
+		Instantiate(playerPrefab[3], playerPrefab[3].transform.position, playerPrefab[3].transform.rotation);
+
+		Instantiate(enemyPrefab, enemyPrefab.transform.position, enemyPrefab.transform.rotation);
+
+		fivePlayer = true;
+	}
+}
---
+++ ChrisJoseph Game Demo/Assets/Scripts/PlayerStateMachine.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateMachine : MonoBehaviour {
+
+	// VARIABLES
+
+	private int camCount = 0;
+
+	public bool canMove = true;
+
+	public GameObject waitingCanvas;
+	public GameObject playingCanvas;
+	public GameObject deadCanvas;
+	public GameObject spectateCanvas;
+
+	public Camera[] cams;
+
+	private PlayerSpawn player;
+
+	public Vector3 offset;
+
+	public enum PlayerStates
+	{
+		WAITING,
+		PLAYING,
+		DEAD,
+		SPECTATING
+	}
+
+	public PlayerStates currentState;
+
+	// FUNCTIONS
+
+	void Start()
+	{
+		currentState = PlayerStates.WAITING;
+
+		player = FindObjectOfType<PlayerSpawn>();
+	}
+
+	void Update ()
+	{
+		cams [1].transform.position = player.playerPrefab [0].transform.position + offset;
+		cams [2].transform.position = player.playerPrefab [1].transform.position + offset;
+		cams [3].transform.position = player.playerPrefab [2].transform.position + offset;
+		cams [4].transform.position = player.playerPrefab [3].transform.position + offset;
+		cams [5].transform.position = player.enemyPrefab.transform.position + offset;
+		
+		switch (currentState) 
+		{
+		case(PlayerStates.WAITING):
+
+			waitingCanvas.SetActive(true);
+			playingCanvas.SetActive(false);
+			deadCanvas.SetActive(false);
+			spectateCanvas.SetActive(false);
+
+			cams [0].enabled = true;
+
+			cams [1].enabled = false;
+			cams [2].enabled = false;
+			cams [3].enabled = false;
+			cams [4].enabled = false;
+			cams [5].enabled = false;
+
+			break;
+
+		case(PlayerStates.PLAYING):
+
+			waitingCanvas.SetActive(false);
+			playingCanvas.SetActive(true);
+			deadCanvas.SetActive(false);
+			spectateCanvas.SetActive(false);
+
+			cams [0].enabled = false;
+
+			cams [1].enabled = true;
+			cams [2].enabled = false;
+			cams [3].enabled = false;
+			cams [4].enabled = false;
+			cams [5].enabled = false;
+
+			break;
+
+		case(PlayerStates.DEAD):
+
+			waitingCanvas.SetActive(false);
+			playingCanvas.SetActive(false);
+			deadCanvas.SetActive(true);
+			spectateCanvas.SetActive(false);
+			
+			break;
+
+		case(PlayerStates.SPECTATING):
+
+			waitingCanvas.SetActive(false);
+			playingCanvas.SetActive(false);
+			deadCanvas.SetActive(false);
+			spectateCanvas.SetActive(true);
+
+			cams [0].enabled = false;
+
+			if(camCount == 1)
+			{
+				cams [1].enabled = false;
+				cams [2].enabled = true;
+				cams [3].enabled = false;
+				cams [4].enabled = false;
+				cams [5].enabled = false;
+			}
+
+			else if(camCount == 2)
+			{
+				cams [1].enabled = false;
+				cams [2].enabled = false;
+				cams [3].enabled = true;
+				cams [4].enabled = false;
+				cams [5].enabled = false;
+			}
+
+			else if(camCount == 3)
+			{
+				cams [1].enabled = false;
+				cams [2].enabled = false;
+				cams [3].enabled = false;
+				cams [4].enabled = true;
+				cams [5].enabled = false;
+			}
+
+			else if(camCount == 4)
+			{
+				cams [1].enabled = false;
+				cams [2].enabled = false;
+				cams [3].enabled = false;
+				cams [4].enabled = false;
+				cams [5].enabled = true;
+			}
+
+			break;
+		}
+	}
+
+	public void NextState()
+	{
+		currentState = (PlayerStates)(((int)currentState + 1) % 5);
+	}
+
+	public void KillPlayer1 ()
+	{
+		Destroy(GameObject.FindGameObjectWithTag("Player1"));
+		NextState();
+	}
+
+	public void NextCam ()
+	{
+		camCount++;
+
+		if (player.twoPlayers == true) 
+		{
+			camCount = 4;
+		} 
+		else if (player.threePlayer == true) 
+		{
+			if (camCount == 2) 
+			{
+				camCount = 4;
+			}
+		}
+		else if (player.fourPlayer == true) 
+		{
+			if (camCount == 3) 
+			{
+				camCount = 4;
+			}
+		}
+
+		if (camCount >= 5) 
+		{
+			camCount = 1;
+		}
+	}
+
+}
